192d70b3879ffdda10c450f0225c53b3c09b6851
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-08-23 21:40:39 -0500

    Completed chapter 3 exercises

---
+++ src/exercises/ch03collections/ArrayListPractice.java
@@ -0,0 +1,70 @@
+package exercises.ch03collections;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListPractice {
+    public static int sumEven(ArrayList<Integer> array) {
+        int total = 0;
+        for (int integer : array) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+
+    public static ArrayList<String> printChars(ArrayList<String> array) {
+        ArrayList<String> exactLetters = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word length: ");
+        int numChars = input.nextInt();
+
+        for (String string : array) {
+            if (string.length() == numChars) {
+                exactLetters.add(string);
+            }
+        }
+        return exactLetters;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> someInts = new ArrayList<>();
+        someInts.add(1);
+        someInts.add(2);
+        someInts.add(3);
+        someInts.add(4);
+        someInts.add(5);
+        someInts.add(6);
+        someInts.add(7);
+        someInts.add(8);
+        someInts.add(9);
+        someInts.add(10);
+
+        int someIntsTotal = sumEven(someInts);
+        System.out.println(someIntsTotal);
+
+        ArrayList<String> someWords = new ArrayList<>();
+        someWords.add("hello");
+        someWords.add("goodbye");
+        someWords.add("school");
+        someWords.add("codes");
+        someWords.add("Java");
+        someWords.add("LaunchCode");
+        someWords.add("Rocks");
+
+        System.out.println(printChars(someWords));
+
+        String sentence = "I would not could not in a box I would not could not with a fox " +
+                "I will not eat them in a house I will not eat them with a mouse";
+        String[] wordArray = sentence.split(" ");
+        ArrayList<String> sentenceWords = new ArrayList<>();
+
+        for (String string : wordArray) {
+            sentenceWords.add(string);
+        }
+        System.out.println(printChars(sentenceWords));
+
+    }
+
+}
---
+++ src/exercises/ch03collections/HashMapPractice.java
@@ -0,0 +1,35 @@
+package exercises.ch03collections;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapPractice {
+    public static void main (String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter student ID (or ENTER to finish: ");
+
+        do {
+            System.out.print("Student Name: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID Number: ");
+                Integer newId = input.nextInt();
+                students.put(newId, newStudent);
+
+                input.nextLine();
+            }
+        } while (!newStudent.equals(""));
+
+        System.out.println("\nClass Roster:");
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println(student.getKey() + " : " + student.getValue());
+        }
+
+    }
+
+
+}
