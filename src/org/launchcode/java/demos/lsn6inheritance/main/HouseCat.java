2c109c2fcbe68b6ec99b58b24c658a527769cd0a
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-09-22 13:17:19 -0500

    Completed Chapter 7 examples

---
+++ src/org/launchcode/java/demos/lsn6inheritance/main/HouseCat.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.demos.lsn6inheritance.main;
+
+public class HouseCat extends Cat {
+    private String name;
+    private String species = "Felis catus";
+
+    public HouseCat(String aName, double aWeight) {
+        super(aWeight);
+        name = aName;
+    }
+
+    public HouseCat(String aName) {
+        name = aName;
+    }
+
+    public boolean isSatisfied() {
+        return !isHungry() && !isTired();
+    }
+
+    @Override
+    public String noise() {
+        if (isSatisfied()) {
+            return "Hello, my name is " + name + "!";
+        } else {
+            return super.noise(); // prints "Meeeeeeooooowww!"
+        }
+    }
+
+    public String purr() {
+        return "I'm a HouseCat";
+    }
+}
